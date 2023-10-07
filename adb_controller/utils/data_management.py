b854bb7de12f8c9abaa9da29f1a170b0fac59cf9
Евгений Вервай <e.vervay@aqsi.ru>
2023-10-07 13:04:12 +0200

    added data_management.py

---
+++ adb_controller/utils/data_management.py
@@ -0,0 +1,17 @@
+import json
+
+def save_data(devices, commands, filename="adb_data.json"):
+    data = {
+        "devices": devices,
+        "commands": commands
+    }
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+def load_data(filename="adb_data.json"):
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
+            return data.get("devices", []), data.get("commands", [])
+    except FileNotFoundError:
+        return [], []
