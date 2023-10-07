de1c544cbdbe83fe18304bf49756dbbb4e5098b6
Евгений Вервай <e.vervay@aqsi.ru>
2023-10-07 16:20:51 +0200

    Added icon

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon  # Добавляем необходимый импорт
 from ui.main_windows import ADBController
 
 if __name__ == "__main__":
+    
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('adb.svg'))
     ex = ADBController()
     sys.exit(app.exec_())
