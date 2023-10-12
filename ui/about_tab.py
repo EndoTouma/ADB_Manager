dd7df87a525ada1acf4c3588595590ffcace43e7
Евгений Вервай <e.vervay@aqsi.ru>
2023-10-13 01:16:57 +0200

    Improved the structure of the code

--- ui/about_tab.py
+++ ui/about_tab.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame, QTextEdit
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame, QTextEdit, QGroupBox
+from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 class AboutTab(QWidget):
@@ -9,43 +10,58 @@ class AboutTab(QWidget):
     def init_ui(self):
         layout_about = QVBoxLayout(self)
 
-        # App Name
-        app_name_label = QLabel("ADB Controller")
-        app_name_font = app_name_label.font()
-        app_name_font.setPointSize(16)
-        app_name_font.setBold(True)
-        app_name_label.setFont(app_name_font)
-        layout_about.addWidget(app_name_label)
+        # App Info Group
+        app_info_group = self.create_group("App Information", self.app_info_ui())
+        layout_about.addWidget(app_info_group)
 
-        # App Version
-        app_version_label = QLabel("Version: 0.0.1")
-        layout_about.addWidget(app_version_label)
+        # Description Group
+        description_group = self.create_group("Description", self.description_ui())
+        layout_about.addWidget(description_group)
 
-        # Horizontal Line
+        layout_about.setAlignment(Qt.AlignTop)
+
+    def create_group(self, title, layout):
+        group = QGroupBox(title)
+        group.setLayout(layout)
+        return group
+
+    def app_info_ui(self):
+        layout = QVBoxLayout()
+
+        # Elements: Labels and Line
+        labels_text = [
+            ("ADB Controller", 16, True),
+            ("Version: 0.0.1", 11, False),
+            ("Author: Eugene Vervai", 11, False),
+            ("Contact: delspin1@gmail.com", 11, False),
+            ("License: MIT License", 11, False)
+        ]
+
+        for text, size, bold in labels_text:
+            label = QLabel(text)
+            font = QFont()
+            font.setPointSize(size)
+            font.setBold(bold)
+            label.setFont(font)
+            layout.addWidget(label)
+
         horizontal_line = QFrame()
         horizontal_line.setFrameShape(QFrame.HLine)
         horizontal_line.setFrameShadow(QFrame.Sunken)
-        layout_about.addWidget(horizontal_line)
+        layout.addWidget(horizontal_line)
 
-        # Author Label
-        author_label = QLabel("Author: Eugene Vervai")
-        layout_about.addWidget(author_label)
+        layout.setSpacing(10)  # consistent spacing
+        layout.setContentsMargins(10, 10, 10, 10)  # consistent margins
 
-        # Contact
-        contact_label = QLabel("Contact: delspin1@gmail.com")
-        layout_about.addWidget(contact_label)
+        return layout
 
-        # License
-        license_label = QLabel("License: MIT License")
-        layout_about.addWidget(license_label)
+    def description_ui(self):
+        layout = QVBoxLayout()
 
-        # Description
-        description_label = QLabel("Description:")
-        layout_about.addWidget(description_label)
-
+        # Description Text
         description_text = QTextEdit()
         description_text.setText(
-            "ADB Controller is a user-friendly application designed to facilitate "
+              "ADB Controller is a user-friendly application designed to facilitate "
             "the management and control of devices via Android Debug Bridge (ADB) "
             "commands. The app allows users to easily add and remove devices and "
             "commands to suit their specific requirements, providing an intuitive "
@@ -60,7 +76,10 @@ class AboutTab(QWidget):
             "managing devices and executing ADB commands effortlessly."
         )
         description_text.setReadOnly(True)
-        layout_about.addWidget(description_text)
 
-        # Adjust spacings, alignments, etc.
-        layout_about.setAlignment(Qt.AlignTop)
+        layout.addWidget(description_text)
+
+        layout.setSpacing(10)  # consistent spacing
+        layout.setContentsMargins(10, 10, 10, 10)  # consistent margins
+
+        return layout
