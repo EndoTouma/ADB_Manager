44c7ed926795e13454d9e7306ae4550a8ef8abca
Евгений Вервай <e.vervay@aqsi.ru>
2024-07-02 02:51:55 +0300

    Update PyQt5 to PyQt6 and set application style
    The upgrade from PyQt5 to PyQt6 is reflected in the import statements. Additionally, a style is set for the application using the setStyle method, with 'WindowsVista' as the selected style. This can be replaced with any available style. Included as well is a minor tweak in the setWindowIcon function parameters for compatibility.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from ui.main_windows import ADBController
-from resources import icons_rc
+import resources.icons_rc  # Это необходимо для иконок
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon(':adb.ico'))
+    app.setWindowIcon(QIcon(':/adb.ico'))
+    
+    # Установка стиля
+    app.setStyle('WindowsVista')  # Можно заменить 'Fusion' на любой доступный стиль
+    
     ex = ADBController()
-    ex.setWindowIcon(QIcon(':adb.ico'))
-    sys.exit(app.exec_())
+    ex.setWindowIcon(QIcon(':/adb.ico'))
+    sys.exit(app.exec())
