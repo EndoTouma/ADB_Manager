cb6b431fa56b5a1b5d9bfe8df0243bac3219be9a
Евгений Вервай <e.vervay@aqsi.ru>
2024-07-04 18:09:09 +0300

    Implement drag-and-drop for APK files in control tab
    Added functionality to enable drag-and-drop of APK files in the control tab. This improvement allows files to be dragged directly into the system, further enhancing usability. The dropped APK file path is extracted and appended into the current command in the command combobox.

--- ui/control_tab.py
+++ ui/control_tab.py
@@ -299,7 +299,7 @@ def create_group(title, layout):
 class ControlTab(QWidget):
     BUTTON_WIDTH = 150
     BUTTON_HEIGHT = 23
-
+    
     def __init__(self, devices, commands):
         super().__init__()
         self.selected_log_level = None
@@ -320,36 +320,51 @@ class ControlTab(QWidget):
         self.highlight_positions = []
         self.current_highlight_index = -1
         self.init_ui()
-
+        
+        self.setAcceptDrops(True)
+    
     def init_ui(self):
         layout_control = QVBoxLayout(self)
         layout_control.addWidget(create_group("Available Devices", self.devices_ui()))
         layout_control.addWidget(create_group("ADB Commands", self.commands_ui()))
         layout_control.addWidget(create_group("Output", self.output_ui()))
-
+        
         self.logcat_button = QPushButton('Start Logcat')
         self.logcat_button.clicked.connect(self.select_log_level_for_logcat)
-
+        
         self.logcat_file_button = QPushButton('Start Logcat to File')
         self.logcat_file_button.clicked.connect(self.select_log_level_for_logcat_to_file)
-
+        
         self.stop_logcat_button = QPushButton('Stop Logcat')
         self.stop_logcat_button.clicked.connect(self.stop_logcat)
-
+        
         logcat_button_layout = QHBoxLayout()
         logcat_button_layout.addWidget(self.logcat_button)
         logcat_button_layout.addWidget(self.logcat_file_button)
         logcat_button_layout.addWidget(self.stop_logcat_button)
-
+        
         layout_control.addLayout(logcat_button_layout)
-
+        
         self.clear_button = QPushButton('Clear Output', self)
         self.clear_button.clicked.connect(self.clear_output)
-
+        
         layout_control.addWidget(self.clear_button)
-
+        
         self.check_device_status()
-
+    
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+    
+    def dropEvent(self, event):
+        urls = event.mimeData().urls()
+        if urls and urls[0].isLocalFile():
+            file_path = urls[0].toLocalFile()
+            if file_path.endswith(".apk"):
+                current_text = self.command_combobox.currentText()
+                new_text = f"{current_text} {file_path}".strip()
+                self.command_combobox.setEditText(new_text)
+    
     def commands_ui(self):
         layout = QVBoxLayout()
         self.command_combobox = QComboBox()
@@ -358,64 +373,66 @@ class ControlTab(QWidget):
         self.command_combobox.setCurrentIndex(-1)
         self.command_combobox.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
         layout.addWidget(self.command_combobox)
-
+        
         command_button_layout = QHBoxLayout()
-
+        
         execute_button = QPushButton('Execute')
         execute_button.clicked.connect(self.execute_adb_command_method)
         command_button_layout.addWidget(execute_button)
-
+        
         add_command_button = QPushButton('Add Command')
         add_command_button.clicked.connect(self.add_command)
         command_button_layout.addWidget(add_command_button)
-
+        
         delete_command_button = QPushButton('Delete Command')
         delete_command_button.clicked.connect(self.delete_command)
         command_button_layout.addWidget(delete_command_button)
-
+        
         plus_button = QPushButton("+")
         plus_button.setFixedWidth(30)
         plus_button.clicked.connect(self.handle_plus_button_click)
         command_button_layout.addWidget(plus_button)
-
+        
         layout.addLayout(command_button_layout)
         return layout
-
+    
     def handle_plus_button_click(self):
         self.select_file_for_install()
-
+    
     def select_file_for_install(self):
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("APK Files (*.apk);;All Files (*)")
         file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
         if file_dialog.exec():
             file_path = file_dialog.selectedFiles()[0]
-            self.command_combobox.setEditText(f"{file_path}")
-
+            current_text = self.command_combobox.currentText()
+            new_text = f"{current_text} {file_path}".strip()
+            self.command_combobox.setEditText(new_text)
+    
     def uninstall_package(self, command):
         package_name, ok = QInputDialog.getText(self, 'Uninstall Package', 'Enter package name to uninstall:')
         if ok and package_name:
             self.command_combobox.setEditText(f"uninstall {package_name}")
-
+    
     def execute_apk_command(self, action, parameter, reinstall=False):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.output_text.append(f"<strong>{action.upper()} COMMAND</strong>: {current_time}\n")
-
+        
         selected_devices = [checkbox.text() for checkbox in self.device_checkboxes if checkbox.isChecked()]
         if not selected_devices:
             QMessageBox.warning(self, "Warning", "Please select at least one device.")
             return
-
+        
         for device in selected_devices:
             thread = CommandThread(device, f"{action} {parameter}", reinstall)
             thread.command_output.connect(self.append_output)
             thread.command_finished.connect(self.command_finished)
             self.command_threads[device] = thread
             thread.start()
-
+    
     def append_output(self, output):
         self.output_text.append(output)
-
+    
     def command_finished(self, device, command, success):
         if success:
             self.output_text.append(f"<strong>COMMAND {command} finished for device: {device}</strong>\n")
@@ -423,7 +440,7 @@ class ControlTab(QWidget):
             self.output_text.append(f"<strong>COMMAND {command} failed for device: {device}</strong>\n")
         if device in self.command_threads:
             del self.command_threads[device]
-
+    
     def refresh_device_list(self):
         device_status, active_devices = get_device_status()
         self.update_device_grid(active_devices)
@@ -431,7 +448,7 @@ class ControlTab(QWidget):
             device_name = checkbox.text()
             status = device_status.get(device_name, "offline")
             update_device_status_ui(checkbox, status)
-
+    
     def update_device_grid(self, active_devices):
         existing_devices = [cb.text() for cb in self.device_checkboxes]
         for device in active_devices:
@@ -440,18 +457,18 @@ class ControlTab(QWidget):
                 self.device_checkboxes.append(checkbox)
                 row, col = divmod(len(self.device_checkboxes) - 1, 4)
                 self.devices_grid.addWidget(checkbox, row, col)
-
+    
     def check_device_status(self):
         for checkbox in self.device_checkboxes:
             device = checkbox.text()
             status = "device" if self.is_device_connected(device) else "offline"
             update_device_status_ui(checkbox, status)
-
+    
     @staticmethod
     def is_device_connected(device):
         result = subprocess.run(['adb', 'devices'], capture_output=True, text=True)
         return device in result.stdout
-
+    
     def devices_ui(self):
         layout = QVBoxLayout()
         self.devices.sort()
@@ -461,42 +478,42 @@ class ControlTab(QWidget):
             row, col = divmod(index, 4)
             self.devices_grid.addWidget(checkbox, row, col)
         layout.addLayout(self.devices_grid)
-
+        
         button_layout = QHBoxLayout()
         select_all_button = QPushButton('Select All')
         select_all_button.clicked.connect(self.select_all_devices)
         button_layout.addWidget(select_all_button)
-
+        
         connect_button = QPushButton('Connect')
         connect_button.clicked.connect(self.connect_devices)
         button_layout.addWidget(connect_button)
-
+        
         disconnect_button = QPushButton('Disconnect')
         disconnect_button.clicked.connect(self.disconnect_devices)
         button_layout.addWidget(disconnect_button)
-
+        
         refresh_button = QPushButton('Refresh Status')
         refresh_button.clicked.connect(self.refresh_device_list)
         button_layout.addWidget(refresh_button)
-
+        
         layout.addLayout(button_layout)
         return layout
-
+    
     def add_command(self):
         dialog = QInputDialog(self)
         dialog.setInputMode(QInputDialog.InputMode.TextInput)
         dialog.setLabelText("Enter ADB Command:")
         dialog.setWindowTitle("Add Command")
         dialog.resize(400, 200)
-
+        
         ok = dialog.exec()
         text = dialog.textValue()
-
+        
         if ok and text:
             self.commands.append(text)
             self.command_combobox.addItem(text)
             DataManager.save_data(self.devices, self.commands)
-
+    
     def delete_command(self):
         dialog = DeleteCommandDialog(self.commands, parent=self)
         if dialog.exec() == QDialog.DialogCode.Accepted:
@@ -505,49 +522,49 @@ class ControlTab(QWidget):
             self.command_combobox.clear()
             self.command_combobox.addItems(self.commands)
             DataManager.save_data(self.devices, self.commands)
-
+    
     def output_ui(self):
         layout = QVBoxLayout()
-
+        
         self.output_text = QTextEdit()
         self.output_text.setReadOnly(True)
         layout.addWidget(self.output_text)
-
+        
         self.highlighter = LogHighlighter(self.output_text.document())
-
+        
         search_button = QPushButton("Search")
         search_button.clicked.connect(self.open_log_viewer)
         layout.addWidget(search_button)
-
+        
         return layout
-
+    
     def open_log_viewer(self):
         log_text = self.output_text.toPlainText()
         run_log_viewer(log_text)
-
+    
     def clear_output(self):
         self.output_text.clear()
         self.highlight_positions = []
         self.current_highlight_index = -1
-
+    
     def connect_devices(self):
         self.execute_device_command("connect")
-
+    
     def disconnect_devices(self):
         self.execute_device_command("disconnect")
-
+    
     def execute_adb_command_method(self):
         self.execute_device_command(self.command_combobox.currentText())
-
+    
     def execute_device_command(self, command):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.output_text.append(f"<strong>{command.upper()} COMMAND</strong>: {current_time}\n")
-
+        
         selected_devices = [checkbox.text() for checkbox in self.device_checkboxes if checkbox.isChecked()]
         if not selected_devices:
             QMessageBox.warning(self, "Warning", "Please select at least one device.")
             return
-
+        
         total_execution_time = 0
         progress_dialog = QProgressDialog(f"{command.capitalize()} devices...", "Cancel", 0, len(selected_devices),
                                           self)
@@ -555,7 +572,7 @@ class ControlTab(QWidget):
         progress_dialog.setAutoClose(False)
         progress_dialog.setAutoReset(False)
         progress_dialog.setMinimumWidth(400)
-
+        
         for i, device in enumerate(selected_devices):
             if progress_dialog.wasCanceled():
                 break
@@ -568,26 +585,26 @@ class ControlTab(QWidget):
             except Exception as e:
                 self.output_text.append(
                     f"<span style='color:red;'><strong>ERROR {command} {device}: {str(e)}</strong></span>\n")
-
+        
         progress_dialog.setValue(len(selected_devices))
         progress_dialog.close()
-
+        
         self.output_text.append(f"<strong>Total {command} time:</strong> {total_execution_time} seconds\n")
         self.output_text.append("<strong>-</strong>" * 120 + "\n")
-
+        
         self.check_device_status()
-
+    
     def select_all_devices(self):
         all_selected = all(cb.isChecked() for cb in self.device_checkboxes)
         for checkbox in self.device_checkboxes:
             checkbox.setChecked(not all_selected)
-
+    
     def select_log_level_for_logcat(self):
         self.log_level_dialog(self.start_logcat)
-
+    
     def select_log_level_for_logcat_to_file(self):
         self.log_level_dialog(self.start_logcat_to_file)
-
+    
     def log_level_dialog(self, callback):
         levels = ['V (Verbose)', 'D (Debug)', 'I (Info)', 'W (Warning)', 'E (Error)', 'F (Fatal)']
         level_descriptions = {
@@ -604,13 +621,13 @@ class ControlTab(QWidget):
             description = level_descriptions[self.selected_log_level]
             QMessageBox.information(self, "Log Level Description", description)
             callback()
-
+    
     def start_logcat(self):
         selected_devices = [checkbox.text() for checkbox in self.device_checkboxes if checkbox.isChecked()]
         if not selected_devices:
             QMessageBox.warning(self, "Warning", "Please select at least one device.")
             return
-
+        
         for device in selected_devices:
             if device not in self.logcat_threads:
                 logcat_thread = LogcatThread(device, log_level=self.selected_log_level)
@@ -619,24 +636,24 @@ class ControlTab(QWidget):
                 self.logcat_threads[device] = logcat_thread
                 logcat_thread.start()
                 self.output_text.append(f"<strong>Started logcat for device: {device}</strong>\n")
-
+    
     def start_logcat_to_file(self):
         selected_devices = [checkbox.text() for checkbox in self.device_checkboxes if checkbox.isChecked()]
         if not selected_devices:
             QMessageBox.warning(self, "Warning", "Please select at least one device.")
             return
-
+        
         file_dialog = QFileDialog(self)
         file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptSave)
         file_dialog.setNameFilter("Text Files (*.txt);;All Files (*)")
         file_dialog.setDefaultSuffix("txt")
         file_dialog.setWindowTitle("Save Logcat Output")
-
+        
         if file_dialog.exec():
             file_path = file_dialog.selectedFiles()[0]
             if not file_path:
                 return
-
+            
             for device in selected_devices:
                 if device not in self.logcat_threads:
                     logcat_thread = LogcatThread(device, log_level="V",
@@ -645,30 +662,30 @@ class ControlTab(QWidget):
                     logcat_thread.finished.connect(self.logcat_finished)
                     logcat_thread.start()
                     self.output_text.append(f"<strong>Started logcat to file for device: {device}</strong>\n")
-
+    
     def stop_logcat(self):
         selected_devices = [checkbox.text() for checkbox in self.device_checkboxes if checkbox.isChecked()]
         if not selected_devices:
             QMessageBox.warning(self, "Warning", "Please select at least one device.")
             return
-
+        
         for device in selected_devices:
             thread = self.logcat_threads.get(device)
             if thread:
                 thread.stop()
                 self.output_text.append(f"<strong>Stopped logcat for device: {device}</strong>\n")
-
+    
     def append_logcat_output(self, output):
         parts = output.split(' ', 4)
         if len(parts) < 5:
             self.output_text.append(f"<span style='color:blue;'>{output}</span>")
             return
-
+        
         timestamp_date, timestamp_time, pid, tid, message = parts[:5]
-
+        
         timestamp_formatted = f"<span style='font-weight:bold;color:#888888;'>{timestamp_date} {timestamp_time}</span>"
         pid_tid_formatted = f"<span style=color:#ff6e00;'>{pid} {tid}</span>"
-
+        
         color = '#a9b7c6'
         if ' E ' in message:
             color = '#cc7832'
@@ -678,10 +695,10 @@ class ControlTab(QWidget):
             color = '#6a8759'
         elif ' D ' in message:
             color = '#6897bb'
-
+        
         self.output_text.append(
             f"{timestamp_formatted} {pid_tid_formatted} <span style='color:{color};'>{message}</span>")
-
+    
     def logcat_finished(self, device):
         self.output_text.append(f"<strong>Logcat finished for device: {device}</strong>\n")
         if device in self.logcat_threads:
