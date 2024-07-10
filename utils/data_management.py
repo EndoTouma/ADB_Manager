baf01f0f0aeb6484faae5c64c725155ea1735254
Евгений Вервай <e.vervay@aqsi.ru>
2024-07-10 20:00:39 +0300

    Update data management utility with error handling and logging
    The data management utility has been enhanced to handle file not found and JSON decoding errors more gracefully. It now defaults to pre-configured settings when unable to load data. Additionally, data saving process has been extended with logging of file content to track outcomes and potential errors during write operations.

--- utils/data_management.py
+++ utils/data_management.py
@@ -1,20 +1,43 @@
 import json
+import os
+
 
 class DataManager:
     @staticmethod
-    def save_data(devices, commands, filename="adb_data.json"):
-        data = {
-            "devices": devices,
-            "commands": commands
-        }
-        with open(filename, "w") as f:
-            json.dump(data, f)
-
-    @staticmethod
     def load_data(filename="adb_data.json"):
+        """ Загрузка данных из файла """
         try:
             with open(filename, "r") as f:
                 data = json.load(f)
-                return data.get("devices", []), data.get("commands", [])
+            return data.get('devices', []), data.get('commands', []), data.get('theme', 'WindowsVista')
         except FileNotFoundError:
-            return [], []
+            print(f"File {filename} not found. Using default settings.")
+            return [], [], 'WindowsVista'
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from {filename}. Using default settings.")
+            return [], [], 'WindowsVista'
+
+    @staticmethod
+    def save_data(devices, commands, theme="WindowsVista", filename="adb_data.json"):
+        data = {
+            "devices": devices,
+            "commands": commands,
+            "theme": theme
+        }
+        print(f"Saving data to {filename}: {data}")
+        try:
+            with open(filename, "w") as f:
+                json.dump(data, f)
+            DataManager.log_file_contents(filename)
+        except Exception as e:
+            print(f"Failed to write to file {filename}: {e}")
+
+    @staticmethod
+    def log_file_contents(filename):
+        """ Логирование содержимого файла после записи """
+        try:
+            with open(filename, "r") as f:
+                content = json.load(f)
+                print(f"Contents of {filename} after write: {content}")
+        except Exception as e:
+            print(f"Failed to read from file {filename}: {e}")
