85ed8c412736c2c5e947a0ac598cc07b5147acca
Евгений Вервай <e.vervay@aqsi.ru>
2024-07-10 20:06:22 +0300

    Add SettingsTab and theme application to the main window
    A new tab, Settings, has been added to the main window's interface and initialized with device, command, and theme details. Additional functionality includes checking the saved theme at startup and applying it accordingly. The theme application logic blocks signals to avoid unnecessary triggers while setting the initial state.

--- ui/main_windows.py
+++ ui/main_windows.py
@@ -1,8 +1,9 @@
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTabWidget
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QApplication
 
 from ui.about_tab import AboutTab
 from ui.control_tab import ControlTab
+from ui.settings_tab import SettingsTab
 from utils.data_management import DataManager
 
 WINDOW_WIDTH = 700
@@ -13,15 +14,20 @@ WINDOW_Y_POS = 100
 
 class ADBController(QWidget):
     
-    def __init__(self):
+    def __init__(self, devices=None, commands=None, theme=None):
         super().__init__()
         
-        self.devices, self.commands = DataManager.load_data()
+        if devices is None or commands is None or theme is None:
+            devices, commands, theme = DataManager.load_data()
+        
+        self.devices = devices
+        self.commands = commands
+        self.theme = theme
         
         self.tab_control = ControlTab(self.devices, self.commands)
+        self.tab_settings = SettingsTab(self.devices, self.commands, self.theme, self)
         
         self.init_ui()
-        
         self.tab_control.refresh_device_list()
     
     def init_ui(self):
@@ -30,6 +36,7 @@ class ADBController(QWidget):
         
         tab_about = AboutTab()
         tabs.addTab(self.tab_control, "Control")
+        tabs.addTab(self.tab_settings, "Settings")
         tabs.addTab(tab_about, "About")
         
         layout.addWidget(tabs)
@@ -39,7 +46,16 @@ class ADBController(QWidget):
         self.setGeometry(WINDOW_X_POS, WINDOW_Y_POS, WINDOW_WIDTH, WINDOW_HEIGHT)
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.setWindowIcon(QIcon('resources/adb.ico'))
+        
+        self.apply_theme()
         self.show()
     
-    def save_data_method(self):
-        DataManager.save_data(self.devices, self.commands)
+    def apply_theme(self):
+        print(f"Applying theme at startup: {self.theme}")
+        QApplication.instance().setStyle(self.theme)
+        should_be_checked = (self.theme == "Fusion")
+        if self.tab_settings.theme_toggle.isChecked() != should_be_checked:
+            self.tab_settings.theme_toggle.blockSignals(True)
+            self.tab_settings.theme_toggle.setChecked(should_be_checked)
+            self.tab_settings.theme_toggle.blockSignals(False)
+        print("Theme applied with state: ", should_be_checked)
